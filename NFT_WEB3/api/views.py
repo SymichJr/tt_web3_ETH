b67621d50d7e6e2e3976b51613b2c10948679c86
Kirill Maksimov <kamaksimov1410@gmail.com>
2023-04-07 14:48:52 +0300

    Updated TokenListView and added TotalSupplyView made several preparations for TokenCreateView

--- NFT_WEB3/api/views.py
+++ NFT_WEB3/api/views.py
@@ -1,14 +1,34 @@
-from rest_framework.decorators import api_view
+import os
+import json
+
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
 
 from nft_token.models import Token
+
 
 from .serializers import TokenSerializer
 
 
-@api_view(['GET'])
-def tokens_list(reqiest):
-    tokens = Token.objects.all()
-    serializer = TokenSerializer(tokens, many=True)
-    return Response(serializer.data)
+class TokenListView(APIView):
+    def get(self, request):
+        tokens = Token.objects.all()
+        serializer = TokenSerializer(tokens, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+class TotalSupplyView(APIView):
+    def get(self, request):
+        web3 = Web3(HTTPProvider(ETH_NODE_URL))
+        web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        contract_address = CONTRACT_ADDRESS
+        with open('abi.json') as file:
+            contract_abi = json.load(file)
+        contract = web3.eth.contract(
+            address=contract_address,
+            abi=contract_abi
+        )
+        total_supply = contract.functions.totalSupply().call()
+        return Response({'result':total_supply}, status=status.HTTP_200_OK)
